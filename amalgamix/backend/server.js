1f5f118d3d954542cd6c454b4983bd376e5dc9c5
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2024-11-04 09:57:55 -0800

    fix: fixed bugs

--- amalgamix/backend/server.js
+++ amalgamix/backend/server.js
@@ -1,11 +1,9 @@
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 import services from "./services.js";
 import express from "express";
 import cors from "cors";
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
