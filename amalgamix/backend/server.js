c66977b1d34c2044b6e53d869b1643bc184c3217
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2024-11-04 10:39:30 -0800

    feat: I connectd the mongoDB to the backend

--- amalgamix/backend/server.js
+++ amalgamix/backend/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import services from "./services.js";
 
+services.connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Hello from Express!' });
 });
 
