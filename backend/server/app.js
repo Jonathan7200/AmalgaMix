dec2ce1e805db5ee9f06707ffe6bb16fdd56f1bc
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2024-11-17 13:17:13 -0800

    feat: just added the mongo connection to app

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,42 +1,40 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-
-// // Environment Variables
-// const PORT = process.env.PORT || 5000;
-// const MONGO_URI = process.env.MONGO_URI;
-
-// // MongoDB Connection
-// mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log('MongoDB connected successfully'))
-// .catch((err) => console.error('MongoDB connection error:', err));
-
-// // Start Server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import apiRoutes from './routes/api.js'; 
+import axios from 'axios';
+import services from './services.js';
+import 'dotenv/config';
 
+services.connectDB();
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', apiRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+// Define an API endpoint that interacts with FastAPI server
+app.post('/get-recommendation', async (req, res) => {
+  try {
+      // Send the song features data to FastAPI server for inference
+      const response = await axios.post('http://localhost:8000/predict', {
+          features: req.body.features  // Pass features from React frontend
+      });
+
+      // Send back the prediction to the client (React)
+      res.json(response.data);
+  } catch (error) {
+      console.error('Error fetching recommendations:', error);
+      res.status(500).json({ error: 'Prediction failed' });
+  }
 });
