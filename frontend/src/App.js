848fba1ae289f83c79eadd6de7ae0f190a2fca3f
Jonathan7200 <jonathan.flores.inbox@gmail.com>
2024-12-02 00:01:20 -0800

    frontend App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,43 @@
-//import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Callback from './components/Callback';
+import PlaylistInput from './components/PlaylistInput';
 
-import './App.css';
-//import Login from './Login'; // Import the Login component
-import TestConnection from './components/TestConnection';
+const App = () => {
+  console.log("app loaded") //testing console log if app loads properly
+  const [accessToken, setAccessToken] = useState(null); //sets token to null
 
 
-function App() {
+  //routes to callback which gets access token
   return (
-    <div className="App">
-   
-      <div>
-        {/* <Login /> */}
-        <TestConnection />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> 
+        <Route
+          path="/callback"
+          element={<Callback setAccessToken={setAccessToken} />}
+        /> 
+        
+
+
+        <Route
+          path="/app"
+          element={
+            accessToken ? (
+              <>
+                {console.log('Access Token in App:', accessToken)} {/* making sure this works check console log */}
+                <PlaylistInput accessToken={accessToken} />
+              </>
+            ) : (
+              <Login />
+            )
+          }
+        />
+
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
