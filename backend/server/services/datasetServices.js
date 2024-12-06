39eff35488323bd02d1a4e5e4967a56017acb62a
legion5184 <96603312+legion5184@users.noreply.github.com>
2024-12-06 00:58:13 -0800

    added datasetServices

---
+++ backend/server/services/datasetServices.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import 'dotenv/config';
+
+
+const datasetServices = {
+    getRecommendations: async (genres) => {
+        try {
+          const response = await axios.post("http://localhost:8000/get-songs", {
+            genres,
+          });
+          return response.data;
+        } catch (error) {
+          console.error("Error communicating with FastAPI:", error.message);
+          throw error;
+        }
+      },
+};
+
+export default datasetServices;
